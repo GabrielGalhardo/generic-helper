34f462f6447a6ff902a616bcdd03d0a406dc5c56
GabrielGalhardo <galhardogabriel843@gmail.com>
2021-12-22 11:37:09 -0300

    first commit

---
+++ generic-helper-app/generic-helper/ClasseHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic_helper
+{
+    class ClasseHelper<T> where T: class
+    {
+        public static string ObterNomeClasse() =>
+            typeof(T).Name;
+        public static MethodInfo ObterMetodoPorNome(string nome) =>
+            typeof(T)
+            .GetMethods()
+            .FirstOrDefault(p => p.Name.Equals(nome, System.StringComparison.InvariantCulture));
+    }
+}
---
+++ generic-helper-app/generic-helper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace generic_helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaBase pessoa = new Pessoa(1, "Gabriel", 20);
+            var metodo = ClasseHelper<Pessoa>.ObterMetodoPorNome("PossuiMaiorIdade");
+            var maiorIdade = metodo.Invoke(pessoa, null);
+            Console.WriteLine($"Possui maior idade:  { maiorIdade}");
+        }
+    }
+    public abstract class PessoaBase { }
+    public class Pessoa : PessoaBase
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(int id, string nome, int idade)
+        {
+            Id = id;
+            Nome = nome;
+            Idade = idade;
+        }
+        public bool PossuiMaiorIdade() =>
+            Idade >= 18;
+    }
+}
