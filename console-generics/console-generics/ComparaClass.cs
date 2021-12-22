83d73afc3a77fd3971d8aebd6ee61eb4013eb057
GabrielGalhardo <galhardogabriel843@gmail.com>
2021-12-22 17:05:05 -0300

    Passando tipo generico na classe

---
+++ console-generics/console-generics/ComparaClass.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_generics
+{
+    public class ComparaClass <T, U>
+    {
+        public void Compara(T p1, U p2) 
+        {
+            bool resultado;
+            resultado = p1.Equals(p2);
+            Console.WriteLine($"Os parâmetros são iguais? {resultado}");
+        }
+    }
+}
---
+++ console-generics/console-generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace console_generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 2;
+            int i2 = 2;
+
+            float f1 = 2;
+            float f2 = 2;
+
+            string s1 = "2";
+            string s2 = "2";
+
+            ComparaClass<int, int> obj1 = new ComparaClass<int, int>();
+            ComparaClass<float, float> obj2 = new ComparaClass<float, float>();
+            ComparaClass<string, string> obj3 = new ComparaClass<string, string>();
+            ComparaClass<string, int> obj4 = new ComparaClass<string, int>();
+            obj1.Compara(i1, i2);
+            obj2.Compara(f1, f2);
+            obj3.Compara(s1, s2);
+            
+            //False -> Equals compara tipo também
+            obj4.Compara(s1, i1);
+
+            Console.ReadKey();
+        }
+
+    }
+}
